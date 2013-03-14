2a65399a01ff01894b8135859941692f5cccddd0
Dean Williams <dean.williams@novatech.co.uk>
2013-03-14 08:10:14 +0000

    Alternative tiling algorithm

--- src/org/codemonkeyism/mdidesktop/MDIDesktopPane.java
+++ src/org/codemonkeyism/mdidesktop/MDIDesktopPane.java
@@ -101,17 +101,49 @@ public class MDIDesktopPane extends JDesktopPane {
 	 * Tile all internal frames
 	 */
 	public void tileFrames() {
-		java.awt.Component[] allFrames = getAllFrames();
-		if (allFrames.length > 0) {
-			manager.setNormalSize();
-			int frameHeight = getBounds().height / allFrames.length;
-			int y = 0;
-			for (int i = 0; i < allFrames.length; i++) {
-				allFrames[i].setSize(getBounds().width, frameHeight);
-				allFrames[i].setLocation(0, y);
-				y = y + frameHeight;
-			}
-		}
+		// How many frames do we have?
+        JInternalFrame[] allframes = getAllFrames();
+        int count = allframes.length;
+        if (count == 0) return;
+        
+        // Determine the necessary grid size
+        int sqrt = (int)Math.sqrt(count);
+        int rows = sqrt;
+        int cols = sqrt;
+        if (rows * cols < count) {
+            cols++;
+            if (rows * cols < count) {
+                rows++;
+            }
+        }
+        
+        // Define some initial values for size & location.
+        Dimension size = getSize();
+        
+        int w = size.width / cols;
+        int h = size.height / rows;
+        int x = 0;
+        int y = 0;
+        
+        // Iterate over the frames, deiconifying any iconified frames and then
+        // relocating & resizing each.
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols && ((i * cols) + j < count); j++) {
+                JInternalFrame f = allframes[(i * cols) + j];
+                
+                if (!f.isClosed() && f.isIcon()) {
+                    try {
+                        f.setIcon(false);
+                    } catch (PropertyVetoException ignored) {}
+                }
+                
+                getDesktopManager().resizeFrame(f, x, y, w, h);
+                x += w;
+            }
+            y += h; // start the next row
+            x = 0;
+        }
+
 	}
 	
 	/**
